1b25d845b887c00a747eb0a3a776d12776d11655
Bryce Nichol <71189882+bryce-n-dev@users.noreply.github.com>
2022-01-29 10:41:59 -0500

    Add screens and react navigation

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, } from '@ui-kitten/components';
+
+import SignUpScreen from './src/screens/SignUpScreen';
+import SignInScreen from './src/screens/SignInScreen';
+import DashboardScreen from './src/screens/DashboardScreen';
+import InputScreen from './src/screens/InputScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Sign In">
+            <Stack.Screen name="Sign In" component={SignInScreen} />
+            <Stack.Screen name="Sign Up" component={SignUpScreen} />
+            <Stack.Screen name="Dashboard" component={DashboardScreen} />
+            <Stack.Screen name="Input" component={InputScreen} />
+          </Stack.Navigator>
+        </NavigationContainer >
+      </ApplicationProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/InputScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const InputScreen = () => {
+  return (
+    <View>
+      <Text>input screen</Text>
+    </View>
+  );
+};
+
+export default InputScreen;
+
+const styles = StyleSheet.create({});
